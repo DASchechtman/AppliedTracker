bd43da7b1f62d56a7e762cdeadc2ea7ef20dfe4d
Daniel Schechtman <dschechtman@carthage.edu>
2023-11-09 14:12:48 -0600

    this code will check once a day to see if it's been a month since I heard back from a company. and if after 30 days the status still isn't yes or no, will set the status for that application to no

--- Code.ts
+++ Code.ts
@@ -1,3 +1,55 @@
-function myFunction() {
+const JOB_DECLINED = "No"
+const JOB_ACCEPTED = "Yes"
+const COMPANY_COL = 0
+const JOB_TITLE_COL = 1
+const STATUS_COL = 6
+
+function GetCompanyCol(data: any[][]) {
+    const FETCHED_DATA = new Array<any>()
+
+    for (let i = 1; i < data.length; i++) {
+        FETCHED_DATA.push(data[i][COMPANY_COL])
+    }
+
+    return FETCHED_DATA
+}
+
+function GetJobStatusCol(data: any[][]) {
+    const FETCHED_DATA = new Array<any>()
+    
+    for (let i = 1; i < data.length; i++) {
+        FETCHED_DATA.push(data[i][STATUS_COL])
+    }
+
+    return FETCHED_DATA
+}
+
+function DaysToMS(days: number) {
+    return days * 86400000
+}
+
+function CheckDeclined() {
+  const SHEET_DATA = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Sheet1")!.getDataRange().getValues()
+
+  const COMPANIES = GetCompanyCol(SHEET_DATA)
   
+  for(let i = 1; i < SHEET_DATA.length; i++) {
+    const COMPANY_NAME = COMPANIES[i]
+    const COMPANY_STATUS = SHEET_DATA[i][STATUS_COL]
+    const JOB_TITLE = SHEET_DATA[i][JOB_TITLE_COL]
+    const PROPS = PropertiesService.getDocumentProperties()
+    const CACHED_COMPANY = PROPS.getProperty(`${COMPANY_NAME}-${JOB_TITLE}`)
+    const MS = Date.now()
+
+    if (CACHED_COMPANY === null) {
+        PROPS.setProperty(`${COMPANY_NAME}-${JOB_TITLE}`, MS.toString())
+        continue
+    }
+
+    if (Date.now() - Number(CACHED_COMPANY) >= DaysToMS(30) && COMPANY_STATUS !== JOB_ACCEPTED && COMPANY_STATUS !== JOB_DECLINED) {
+        SHEET_DATA[i][STATUS_COL] = JOB_DECLINED
+    }
+  }
+
+  SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Sheet1")!.getDataRange().setValues(SHEET_DATA)
 }
